ee8dc5ab119bf60c0c32d0aa095bc6b5595af376
Susiniosgit <magnawlege@gmail.com>
2021-10-04 07:55:28 -0700

    main.h corrected

--- _printf.c
+++ _printf.c
@@ -1,71 +1,82 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * _printf - function that produces output according to a format.
- * @format: input argument types for output format.
- * Return: the number of characters printed or in case of error -1.
+ * check_for_specifiers - checks if there is a valid format specifier
+ * @format: possible format specifier
+ *
+ * Return: pointer to valid function or NULL
+ */
+static int (*check_for_specifiers(const char *format))(va_list)
+{
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"u", print_u},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
+	};
+
+	for (i = 0; p[i].t != NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+}
+
+/**
+ * _printf - prints anything
+ * @format: list of argument types passed to the function
+ *
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, j = 0;
-	va_list arg;
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
 
-	va_start(arg, format);
-
-	if (!format || (format[0] == '%' && format[1] == '\0'))
-	{
+	if (format == NULL)
 		return (-1);
-	}
-	for (; format[i] != '%' && format[i] != '\0'; i++, j++)
+	va_start(valist, format);
+	while (format[i])
 	{
-		_putchar(format[i]);
-	}	
-	switch (format[i + 1])
-	{
-		case 'c':
-			i++;
-			j += print_c(arg);
-			i++;
-			break;
-		case 's':
-			i++;
-			j += print_s(arg);
-			i++;
-			break;
-		case 'd':
-			i++;
-			j += print_numd(arg);
-			i++;
-			break;
-		case 'i':
-			i++;
-			j += print_numd(arg);
-			i++;
-			break;
-		case 'o':
-			i++;
-			j += print_numo(arg);
-			i++;
-			break;
-		case 'x':
-			i++;
-			j += print_numx(arg);
-			i++;
-			break;
-		case 'u':
-			i++;
-			j += print_numu(arg);
-			i++;
-			break;
-		default:
-			break;
-		i++;
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			count++;
 		}
-	while (format[i] != '\0')
-	{
+		if (!format[i])
+			return (count);
+		f = check_for_specifiers(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
 		_putchar(format[i]);
-		j++;
-		i++;
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
 	}
-	va_end(arg);
-	return (j);
+	va_end(valist);
+	return (count);
 }
